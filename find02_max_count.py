371cdc667eba5ae0531763c0a54ba11781341ef8
yusupovna1403 <yusupovna2003girl@gmail.com>
2023-01-08 12:40:02 +0500

    list_02

--- find02_max_count.py
+++ find02_max_count.py
@@ -5,4 +5,17 @@ def find_max_count(data):
         data: list of numbers
     returns: count of maximum numbers in the list
     """
-    return 0
+
+    i = 1
+    s = 0
+    max = data[0]
+    while i < len(data):
+        if data[i] > max:
+            max = data[i]
+        i+=1
+    while data:
+        if max == data.pop():
+            s+=1
+    return  s
+data = [1,2,6,9,4,5,9]
+print(find_max_count(data))
