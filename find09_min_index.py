00de412b34b2fa278bc739ddd1df6c1ec1ee68fd
yusupovna1403 <yusupovna2003girl@gmail.com>
2023-01-08 22:07:24 +0500

    list_09

--- find09_min_index.py
+++ find09_min_index.py
@@ -5,5 +5,14 @@ def find_min_index(data):
         data: list of numbers
     returns: index of minimum number in the list
     """
-    return 0
+    i = 1
+    min = data[0]
+    while i < len(data):
+        if data[i] < min:
+            min = data[i]
+        i+=1
+    min_index = data.index(min)
+    return  min_index
+data = [1,2,6,9,-4,5,9]
+print(find_min_index(data))
 
