e48282e262af506f68797ef66d23f8839af1472b
yusupovna1403 <yusupovna2003girl@gmail.com>
2023-01-08 19:24:42 +0500

    list_04

--- find04_max_index.py
+++ find04_max_index.py
@@ -5,4 +5,15 @@ def find_max_index(data):
         data: list of numbers
     returns: index of maximum number in the list
     """
-    return 0
+
+    i = 1
+    max = data[0]
+    while i < len(data):
+        if data[i] > max:
+            max = data[i]
+        i+=1
+    
+    return  data.index(max)
+data = [1,2,6,4,5,9]
+print(find_max_index(data))
+    
