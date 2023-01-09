4394266bfe67200a1acc7756eb2a6c18bbfc9da7
yusupovna1403 <yusupovna2003girl@gmail.com>
2023-01-09 18:26:46 +0500

    list_06

--- find06_max_odd.py
+++ find06_max_odd.py
@@ -5,4 +5,32 @@ def find_max_odd(data):
         data: list of numbers
     returns: maximum odd number in the list
     """
-    return 0
+    i = 0
+    a = []
+
+    while i < len(data):
+        
+        if data[i] % 2 == 1:
+            a.append(data[i])
+            
+        i+=1
+    if len(a) == 0:
+        return -1
+    else:
+        j = 0
+        max = a[0]
+        while j < len(a):
+            if a[j] > max:
+                max = a[j]
+            j+=1
+        return max
+
+            
+            
+
+    
+data = [11,4,7,8,2]
+print(find_max_odd(data))
+
+            
+            
